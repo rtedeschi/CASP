0d9da497a7496d6826bf9ac278a79a52bfec8b61
Ryan Tedeschi <ryanat13@gmail.com>
2017-01-04 01:54:14 -0500

    More automation updates

--- Core/src/Timestamp.h
+++ Core/src/Timestamp.h
@@ -1,3 +1,7 @@
+/*
+* It is not advised to edit this file, as it
+* is automatically generated with every build
+*/
 #ifndef TIMESTAMP
-#define TIMESTAMP "01-04-2017 01:47:05"
+#define TIMESTAMP "01-04-2017 01:53:07"
 #endif
