f58469f03b8519d616aaa4e4723579eb3099d692
Ryan Tedeschi <ryanat13@gmail.com>
2017-03-28 14:21:01 -0400

    Decisions in c++

--- Core/sample/source.cpp
+++ Core/sample/source.cpp
@@ -1,11 +1,8 @@
-for (int i = 0; i < 7; i++) {
-    some_function(i);
-}
-
-int sample(int n) {
-
-    int p = 0;
-
-    some_function(n);
-
+if (some_var > 7) {
+    do_this();
+} 
+else if (some_var > 4)
+    do_else();
+else {
+    do_that(some_var);
 }
